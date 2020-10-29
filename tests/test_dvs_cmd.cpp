aa210faccd45c1e193d5b0f469b35f94783985a3
David Summers <david@summersoft.fay-ar.us>
2020-10-29 00:17:08 -0500

    Add test for internal hash command.

--- tests/test_dvs_cmd.cpp
+++ tests/test_dvs_cmd.cpp
@@ -7,6 +7,7 @@
 #include <windows.h>
 #endif
 
+#include <fstream>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
@@ -49,6 +50,33 @@ static dvs_error_t test_dvs_init( )
 }
 
 
+// Tests the 'dvs internal hash README.txt' command.
+static dvs_error_t test_dvs_internal_hash( )
+{
+  std::filesystem::remove_all( "test" );
+  {
+    DVS dvs;
+
+    InitCommand initCommand;
+
+    std::string err = initCommand.InitDvs( dvs, "test" );
+
+    if ( !err.empty( ) )
+    {
+      std::stringstream ss;
+      ss << "Error creating dvs 'test' directory: " << err << std::endl; 
+      DVS_ERROR( ss.str( ).c_str( ) );
+    }
+
+    std::filesystem::current_path( "test" );
+
+    std::ofstream readmeFile( "README.txt" );
+    readmeFile << "This is a README.txt file." << std::endl;
+  }
+
+  return DVS_NO_ERROR;
+}
+
 
 /* The test table.  */
 
@@ -56,5 +84,6 @@ struct dvs_test_descriptor_t test_funcs[] =
 {
   DVS_TEST_NULL,
   DVS_TEST_PASS( test_dvs_init, "Test DVS init command." ),
+  DVS_TEST_PASS( test_dvs_init, "Test DVS internal hash command." ),
   DVS_TEST_NULL
 };
