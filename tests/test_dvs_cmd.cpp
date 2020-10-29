089a1c5cf997ce0357a147cf0af5026e03983ee6
David Summers <david@summersoft.fay-ar.us>
2020-10-29 00:41:54 -0500

    Test HashCommand to make sure it gives correct hash.

--- tests/test_dvs_cmd.cpp
+++ tests/test_dvs_cmd.cpp
@@ -24,24 +24,28 @@
 #include "dvs.h"
 #include "dvs_test.h"
 #include "command_init.h"
+#include "command_hash.h"
+
+
+const char *TEST_DIR = "test";
 
 // The tests.
 
 // Tests the 'dvs init' command.
 static dvs_error_t test_dvs_init( )
 {
-  std::filesystem::remove_all( "test" );
+  std::filesystem::remove_all( TEST_DIR );
   {
     DVS dvs;
 
     InitCommand initCommand;
 
-    std::string err = initCommand.InitDvs( dvs, "test" );
+    std::string err = initCommand.InitDvs( dvs, TEST_DIR );
 
     if ( !err.empty( ) )
     {
       std::stringstream ss;
-      ss << "Error creating dvs 'test' directory: " << err << std::endl; 
+      ss << "Error creating dvs '" << TEST_DIR << "' directory: " << err << std::endl; 
       DVS_ERROR( ss.str( ).c_str( ) );
     }
   }
@@ -53,25 +57,46 @@ static dvs_error_t test_dvs_init( )
 // Tests the 'dvs internal hash README.txt' command.
 static dvs_error_t test_dvs_internal_hash( )
 {
-  std::filesystem::remove_all( "test" );
+  std::filesystem::remove_all( TEST_DIR );
   {
     DVS dvs;
 
     InitCommand initCommand;
 
-    std::string err = initCommand.InitDvs( dvs, "test" );
+    std::string err = initCommand.InitDvs( dvs, TEST_DIR );
 
     if ( !err.empty( ) )
     {
       std::stringstream ss;
-      ss << "Error creating dvs 'test' directory: " << err << std::endl; 
+      ss << "Error creating dvs '" << TEST_DIR << "' directory: " << err << std::endl; 
       DVS_ERROR( ss.str( ).c_str( ) );
     }
 
-    std::filesystem::current_path( "test" );
+    std::filesystem::current_path( TEST_DIR );
 
-    std::ofstream readmeFile( "README.txt" );
-    readmeFile << "This is a README.txt file." << std::endl;
+    const char *README = "README.txt";
+
+    {
+      std::ofstream readmeFile( README );
+      readmeFile << "This is a " << README << " file." << std::endl;
+    }
+
+    HashCommand hashCommand;
+
+    OidResult result = hashCommand.Hash( dvs, README );
+    
+    if ( !result.err.empty( ) )
+    {
+      DVS_ERROR( result.err.c_str( ) );
+    }
+
+    const char *EXPECTED_OID = "996d18c4220e5b4386e1ddeba480b2fc6e54e4d986ecf3634b834122c7fafdff";
+    if ( result.oid != EXPECTED_OID )
+    {
+      std::stringstream ss;
+      ss << "Expected: " << EXPECTED_OID << ", but got " << result.oid << std::endl;
+      DVS_ERROR( ss.str( ).c_str( ) );
+    }
   }
 
   return DVS_NO_ERROR;
@@ -83,7 +108,7 @@ static dvs_error_t test_dvs_internal_hash( )
 struct dvs_test_descriptor_t test_funcs[] =
 {
   DVS_TEST_NULL,
-  DVS_TEST_PASS( test_dvs_init, "Test DVS init command." ),
-  DVS_TEST_PASS( test_dvs_init, "Test DVS internal hash command." ),
+  DVS_TEST_PASS( test_dvs_init,          "Test DVS init command."          ),
+  DVS_TEST_PASS( test_dvs_internal_hash, "Test DVS internal hash command." ),
   DVS_TEST_NULL
 };
