f896b1e66e92fd7fe8da20b0c307630914446113
David Summers <david@summersoft.fay-ar.us>
2020-10-30 22:54:38 -0500

    First implementation of 'log' command.

--- src/command_log.cpp
+++ src/command_log.cpp
@@ -3,8 +3,8 @@
 #include <sstream>
 #include <string.h>
 
+#include "command_cat.h"
 #include "command_log.h"
-#include "command_hash.h"
 #include "dvs.h"
 
 
@@ -16,12 +16,6 @@ std::string LogCommand::ParseArgs( std::map< std::string, docopt::value > &args_
        hashOption && hashOption.isString( ) && !hashOption.asString( ).empty( ) )
   {
       m_HashId = hashOption.asString( );
-  }
-  else
-  {
-      std::stringstream ss;
-      ss << "Missing hash identifier.";
-      err = ss.str( );
   }
 
   return err;
@@ -53,5 +47,91 @@ std::string LogCommand::GetLog( DVS &dvs_, const std::string &hashId_ )
     hashId = dvs_.GetHead( );
   }
 
+  while ( !hashId.empty( ) )
+  {
+    CatCommand catCommand;
+    std::stringstream commitSs;
+
+    CatCommand::CatResult catResult = catCommand.GetHash( dvs_, hashId, &commitSs );
+
+    if ( !catResult.err.empty( ) )
+    {
+      result = catResult.err;
+      return result;
+    }
+
+    if ( catResult.type != "commit" )
+    {
+      std::stringstream ss;
+      ss << "Dvs log command expected 'commit' type for Hash ID '" << hashId << "'" << std::endl;
+      return ss.str( );
+    }
+
+    std::string treeHash;
+    std::string parentHash;
+    std::string msg;
+
+    std::string type;
+    std::string hash;
+
+    // Get the tree hash and the parent hash (if any).
+    while ( true )
+    {
+      std::string line;
+
+      std::getline( commitSs, line );
+
+      size_t pos = line.find( ' ' );
+      if ( pos == std::string::npos )
+      {
+        break;
+      } 
+
+      type = line.substr( 0, pos );
+      hash = line.substr( pos + 1 );
+
+      if ( type.empty( ) && hash.empty( ) )
+      {
+        break;
+      }
+
+      if ( type == "tree" )
+      {
+        treeHash = hash;
+      }
+      else if ( type == "parent" )
+      {
+        parentHash = hash;
+      }
+      else
+      {
+        std::stringstream ss;
+        ss << "Log command: Unknown type '" << type << "'." << std::endl;
+        return ss.str( );
+      }
+    }
+
+    while ( true )
+    {
+      std::string input;
+      std::getline( commitSs, input );
+
+      if ( input.empty( ) )
+      {
+        break;
+      }
+
+      msg += input;
+    }
+
+    std::cout << "commit " << treeHash << std::endl;
+    std::cout << std::endl;
+    std::cout << msg << std::endl;
+    std::cout << std::endl;
+
+    hashId = parentHash;
+  }
+
+  // The rest of the contents of the commit is the commit message;
   return result;
 }
